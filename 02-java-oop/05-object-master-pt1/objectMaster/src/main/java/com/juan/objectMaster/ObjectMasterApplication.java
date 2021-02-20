0235339fea63fcf1ee65dc60fafa41ef131f75b4
Juan Renteria <Juan@Juans-MacBook-Air.local>
2021-02-19 20:50:48 -0800

    object master 1

---
+++ 02-java-oop/05-object-master-pt1/objectMaster/src/main/java/com/juan/objectMaster/ObjectMasterApplication.java
@@ -0,0 +1,13 @@
+package com.juan.objectMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectMasterApplication.class, args);
+	}
+
+}
---
+++ 02-java-oop/05-object-master-pt1/objectMaster/src/test/java/com/juan/objectMaster/ObjectMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.objectMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
